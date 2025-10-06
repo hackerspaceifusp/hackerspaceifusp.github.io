7126e64a79d6bdfae9916a5187a9195a63a49fbf
rafael-valiati <44684423+rafael-valiati@users.noreply.github.com>
2025-10-06 17:02:56 +0200

    Update mapa_estacoes.py

--- mapa_estacoes.py
+++ mapa_estacoes.py
@@ -145,7 +145,7 @@ for idx, row in gdf.iterrows():
             ax.text(row.geometry.x, row.geometry.y + 16, f"Gramado", color='black', va='center', ha='center', fontsize=15, weight='bold')
         elif idx in [1]:
             ax.text(row.geometry.x, row.geometry.y - 16, f"Pelletron - topo", color='black', va='center', ha='center', fontsize=15, weight='bold')
-        if (33 <= row['Temperatura'] < 40) or (-5 < row['Temperatura'] <= 5):
+        if (32 <= row['Temperatura'] < 40) or (-5 < row['Temperatura'] < 8):
             ax.text(row.geometry.x, row.geometry.y, f'{row["Temperatura"]:.1f}', color='white', ha='center', va='center', fontsize=18, weight='bold')
         else:
             ax.text(row.geometry.x, row.geometry.y, f'{row["Temperatura"]:.1f}', color='black', ha='center', va='center', fontsize=18, weight='bold')
